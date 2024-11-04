4299134e1f2fef79122ab65fc3b34f175a2a51e4
Sébastien VIAL <sebastien@shyrogan.fr>
2024-11-04 10:26:29 +0100

    fix: `<>` dans les identifiants par défaut de la CI

--- scripts/build_flags_wifi.py
+++ scripts/build_flags_wifi.py
@@ -3,14 +3,14 @@ Import("env")
 import os
 
 # Set default values if environment variables are missing
-wifi_ssid = os.getenv("WIFI_SSID", "<CI>")
-wifi_password = os.getenv("WIFI_PASSWORD", "<CI>")
+wifi_ssid = os.getenv("WIFI_SSID", "CI")
+wifi_password = os.getenv("WIFI_PASSWORD", "CI")
 
 # Pass these as build flags
 env.Append(
     BUILD_FLAGS=[
-        f'-D WIFI_SSID="{wifi_ssid}"',
-        f'-D WIFI_PASSWORD="{wifi_password}"'
+        f'-D WIFI_SSID="\\"{wifi_ssid}\\""',
+        f'-D WIFI_PASSWORD="\\"{wifi_password}\\""'
     ]
 )
 

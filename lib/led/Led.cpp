a98d51c81661753914017c987246aa14521972ed
Sébastien <sebastien@shyrogan.fr>
2024-12-11 10:16:00 +0100

    feat: `led` module

---
+++ lib/led/Led.cpp
@@ -0,0 +1,35 @@
+#include "Led.h"
+#include "esp32-hal-adc.h"
+#include "esp32-hal.h"
+#include <stdint.h>
+
+Led::Led(uint8_t ledPin, uint8_t photocellPin) {
+  this->_ledPin = ledPin;
+  this->_photocellPin = photocellPin;
+  this->_automatic = false;
+}
+
+bool Led::is_automatic() {
+  return this->_automatic;
+}
+
+void Led::set_automatic(bool automatic) {
+  this->_automatic = automatic;
+}
+
+double Led::get_luminosity() {
+  if (!this->is_automatic())
+    return this->_luminosity;
+  uint16_t hue = analogRead(this->_photocellPin);
+  return (double)hue / 4095.0;
+}
+
+void Led::set_luminosity(double value) {
+  this->_luminosity = value;
+}
+
+void Led::loop() {
+  analogWrite(LED_PIN, (int) (this->get_luminosity()  * 255));
+}
+
+Led led = Led(LED_PIN, PHOTOCELL_PIN);

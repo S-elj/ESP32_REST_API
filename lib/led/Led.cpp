b2aa443e806a04d832713983916b3a05ef827b27
Sébastien <sebastien@shyrogan.fr>
2025-01-09 09:59:44 +0100

    feat: threshold for led, more routes

--- lib/led/Led.cpp
+++ lib/led/Led.cpp
@@ -3,36 +3,42 @@
 #include "esp32-hal.h"
 #include <stdint.h>
 
+double MAX_VALUE = 4095.0;
+
 Led::Led(uint8_t ledPin, uint8_t photocellPin) {
   this->_ledPin = ledPin;
   this->_photocellPin = photocellPin;
+  this->_threshold = -1;
   this->_automatic = false;
 }
 
-bool Led::is_automatic() {
-  return this->_automatic;
-}
+bool Led::is_automatic() { return this->_automatic; }
 
-void Led::set_automatic(bool automatic) {
-  this->_automatic = automatic;
-}
+void Led::set_automatic(bool automatic) { this->_automatic = automatic; }
 
 double Led::get_luminosity() {
   if (!this->is_automatic())
-    return this->_luminosity;
-  uint16_t hue = analogRead(this->_photocellPin);
-  return (double)hue / 4095.0;
+    return (double)analogRead(this->_photocellPin) / MAX_VALUE;
+  return this->_luminosity;
 }
 
 void Led::set_luminosity(double value) {
   if (this->is_automatic()) {
     this->set_automatic(false);
   }
-  this->_luminosity = value;
+  if (this->get_threshold() * MAX_VALUE <= get_luminosity())
+    this->_luminosity = value;
+  else
+    this->_luminosity = 0.0;
 }
 
-void Led::loop() {
-  analogWrite(LED_PIN, (int) (this->get_luminosity()  * 255));
+void Led::set_threshold(double threshold) {
+  if (this->is_automatic()) {
+    this->set_automatic(false);
+  }
+  this->_threshold = threshold;
 }
+
+void Led::loop() { analogWrite(LED_PIN, (int)(this->get_luminosity() * 255)); }
 
 Led led = Led(LED_PIN, PHOTOCELL_PIN);
--- lib/routes/Routes.h
+++ lib/routes/Routes.h
@@ -1,29 +1,32 @@
-#include <ESPAsyncWebServer.h>
 #include <AsyncJson.h>
+#include <ESPAsyncWebServer.h>
 #include <SPIFFS.h>
 
 namespace routes {
-  namespace oapi {
-    // Renvoie le schéma OpenAPI de l'API pour le projet, disponible dans le dossier `/data/openapi.yml`.
-    // Il faut noter que certains paramètres de l'API sont inclus dynamiquement (notamment la version, cf. implémentation).
-    void handle_oapi_schema(AsyncWebServerRequest *request);
-    
-    // Renvoie une interface graphique à l'aide de [Scalar](https://scalar.vector.dev/).
-    void handle_scalar(AsyncWebServerRequest *request);
-  }
-  namespace monitoring {
-    // Gère la route `/v1/` comme définie dans le schéma OAPI.
-    void handle_root(AsyncWebServerRequest *request);
-  }
-  namespace sensors {
-    // Renvoie les métadonnées à propos du photocell, généralement placé au pin 36.
-    void handle_photocell_sensor(AsyncWebServerRequest *request);
-  }
-  namespace mechanical {
-    void handle_buzzer_stop(AsyncWebServerRequest *request);
-    void handle_buzzer_activate(AsyncWebServerRequest *request);
-    void handle_led_get_state(AsyncWebServerRequest *request);
-    void handle_led_set_automatic(AsyncWebServerRequest *request);
-    void handle_led_set_luminosity(AsyncWebServerRequest *request);
-  }
-}
+namespace oapi {
+// Renvoie le schéma OpenAPI de l'API pour le projet, disponible dans le dossier
+// `/data/openapi.yml`. Il faut noter que certains paramètres de l'API sont
+// inclus dynamiquement (notamment la version, cf. implémentation).
+void handle_oapi_schema(AsyncWebServerRequest *request);
+
+// Renvoie une interface graphique à l'aide de
+// [Scalar](https://scalar.vector.dev/).
+void handle_scalar(AsyncWebServerRequest *request);
+} // namespace oapi
+namespace monitoring {
+// Gère la route `/v1/` comme définie dans le schéma OAPI.
+void handle_root(AsyncWebServerRequest *request);
+} // namespace monitoring
+namespace sensors {
+// Renvoie les métadonnées à propos du photocell, généralement placé au pin 36.
+void handle_photocell_sensor(AsyncWebServerRequest *request);
+} // namespace sensors
+namespace mechanical {
+void handle_buzzer_stop(AsyncWebServerRequest *request);
+void handle_buzzer_activate(AsyncWebServerRequest *request);
+void handle_led_get_state(AsyncWebServerRequest *request);
+void handle_led_set_automatic(AsyncWebServerRequest *request);
+void handle_led_set_luminosity(AsyncWebServerRequest *request);
+void handle_led_set_threshold(AsyncWebServerRequest *request);
+} // namespace mechanical
+} // namespace routes
